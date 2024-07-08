78b66b6c8454010863f51727a83251e49d9b4135
gamal <gamal@gamalvm.myguest.virtualbox.org>
2024-07-08 18:31:20 +0300

    init terraform

---
+++ terraform/s3-backend.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "me-central-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
